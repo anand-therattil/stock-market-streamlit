c9ab9491a51905d131786fec02a06cf3cd9e934f
Anand Therattil <65058094+anand-therattil@users.noreply.github.com>
2024-06-23 17:39:09 +0530

    Add files via upload
    Random Forrest Implementation  the model is also loaded

--- frontend.py
+++ frontend.py
@@ -5,6 +5,9 @@ import yfinance as yf
 import pandas as pd 
 import plotly.graph_objects as go
 
+# Model Initialization and Training
+from sklearn.ensemble import RandomForestClassifier
+import joblib
 
 # Get the historical Data for the particular stock 
 def get_stock_data(stock_name, start_date, end_date):
@@ -23,7 +26,12 @@ def get_stock_data(stock_name, start_date, end_date):
     data = yf.download(stock_ticker, start=start_date, end=end_date)
     return  data
 
-
+    # Data Preparation
+def pre_process_data(data):
+    data["open/Close"] = data['Open'] / data['Close']
+    data["Volume/High"] = data['Volume'] / data['High']
+    data['Volume/Low'] = data['Volume'] / data['Low']
+    return data
 
 with st.form( key="Initial Data", border=True):
     st.header("Stock Analytics")
@@ -44,8 +52,14 @@ if(submit == True):
         data = get_stock_data(stock_name, date_range[0], date_range[1])
         date = data.index
         
+        data['Tomorrow']  = data['Close'].shift(-1)
+        data = data.dropna()
+        data['Price Change'] = round((data.loc[:,'Close'] - data.loc[:,'Tomorrow'])*100/data['Close'],2)
+        display_header = data.columns.tolist()
+        display_header.remove("Tomorrow")
+        
         st.subheader("Historic Data")
-        st.dataframe(data.iloc[:,1:],use_container_width=True)
+        st.dataframe(data.loc[:,display_header],use_container_width=True)
         
         st.subheader("Prices Variation")
         st.line_chart(data.loc[:,['Close','Open']])
@@ -57,3 +71,21 @@ if(submit == True):
                 close=data['Close'])])
 
         st.plotly_chart(fig)
+        
+        processed_data = pre_process_data(data)
+        predictors = processed_data.columns.tolist()
+        predictors.remove("Tomorrow")
+        predictors.remove("Price Change")
+        
+        model = RandomForestClassifier(n_estimators=len(predictors), min_samples_split=len(predictors), random_state=1)
+
+        model = joblib.load(r"C:\Users\Anand\Desktop\Coding\projects\stock_market_prediction\models\RandomForrest.pkl")
+        last_date_data = processed_data.iloc[-1]
+        last_date_data = [last_date_data.loc[predictors].values]
+        predicted_value = model.predict(last_date_data)
+
+        st.subheader("Tommorow Prediction Random Forrest ")
+        if(predicted_value[0]==0):
+            st.info("Price Goes Down",  icon=":material/thumb_down:")      
+        else:
+            st.info("Price Goes Up",  icon=":material/thumb_up:")
