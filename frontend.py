acf15488f54aa5f89ba847bc9f13f0500b422174
Anand Therattil <65058094+anand-therattil@users.noreply.github.com>
2024-06-24 22:27:29 +0530

    Add files via upload
    Added the tab in UI and few fundamental financials

--- frontend.py
+++ frontend.py
@@ -26,6 +26,14 @@ def get_stock_data(stock_name, start_date, end_date):
     data = yf.download(stock_ticker, start=start_date, end=end_date)
     return  data
 
+def get_fundamental_financials(stock_name):
+
+    region = "NS"
+    stock_ticker = stock_name.upper()+"."+region.upper()
+    fundamental_data = yf.Ticker(stock_ticker).info
+    
+    return fundamental_data
+
     # Data Preparation
 def pre_process_data(data):
     data["open/Close"] = data['Open'] / data['Close']
@@ -33,7 +41,7 @@ def pre_process_data(data):
     data['Volume/Low'] = data['Volume'] / data['Low']
     return data
 
-with st.form( key="Initial Data", border=True):
+with st.sidebar:
     st.header("Stock Analytics")
     stock_name = st.selectbox("Please Select Stock Name",
                           ("SUZLON", "TATAMOTORS", "ONGC"))
@@ -44,48 +52,74 @@ with st.form( key="Initial Data", border=True):
         (today, today),
         format="DD.MM.YYYY",
     )
-    submit = st.form_submit_button("Fetch Data",use_container_width=True)
+    
+st.title(stock_name)
 
-if(submit == True):    
-    with st.container(border = True):
-        st.subheader(stock_name)
-        data = get_stock_data(stock_name, date_range[0], date_range[1])
-        date = data.index
-        
-        data['Tomorrow']  = data['Close'].shift(-1)
-        data = data.dropna()
-        data['Price Change'] = round((data.loc[:,'Close'] - data.loc[:,'Tomorrow'])*100/data['Close'],2)
-        display_header = data.columns.tolist()
-        display_header.remove("Tomorrow")
-        
-        st.subheader("Historic Data")
-        st.dataframe(data.loc[:,display_header],use_container_width=True)
-        
-        st.subheader("Prices Variation")
-        st.line_chart(data.loc[:,['Close','Open']])
-                
-        fig = go.Figure(data=[go.Candlestick(x=date,
-                open=data['Open'],
-                high=data['High'],
-                low=data['Low'],
-                close=data['Close'])])
+financials, historic_data, charts = st.tabs(["Financials", "Historic Data", "Charts"])
+with financials:
+    fundamental_financials = get_fundamental_financials(stock_name)
+    st.info(fundamental_financials['longBusinessSummary'],icon=":material/info:")
+    st.link_button("Suzlon website", fundamental_financials['website'])
 
-        st.plotly_chart(fig)
+    # st.json(fundamental_financials)
+    EBITDA, D2E, MCap = st.columns(3,gap="small")
+    
+    with EBITDA:
+        st.metric(label="EBITDA",value=fundamental_financials['ebitda'])
+    with D2E:
+        st.metric(label="Debt To Equity",value=fundamental_financials['debtToEquity'])
+    with MCap:
+        st.metric(label="Market Cap",value=fundamental_financials['marketCap'])
         
-        processed_data = pre_process_data(data)
-        predictors = processed_data.columns.tolist()
-        predictors.remove("Tomorrow")
-        predictors.remove("Price Change")
-        
-        model = RandomForestClassifier(n_estimators=len(predictors), min_samples_split=len(predictors), random_state=1)
+    BV, H52, L52 = st.columns(3,gap="small")
+    
+    with BV:
+        st.metric(label="Book Value",value=fundamental_financials['bookValue'])
+    with H52:
+        st.metric(label="52 Week High",value=fundamental_financials['fiftyTwoWeekHigh'])
+    with L52:
+        st.metric(label="52 Week Low",value=fundamental_financials['fiftyTwoWeekLow'])
+    
+with historic_data:
+    
+    data = get_stock_data(stock_name, date_range[0], date_range[1])
+    date = data.index
+    
+    data['Tomorrow']  = data['Close'].shift(-1)
+    data = data.dropna()
+    data['Price Change'] = round((data.loc[:,'Close'] - data.loc[:,'Tomorrow'])*100/data['Close'],2)
+    display_header = data.columns.tolist()
+    display_header.remove("Tomorrow")
+    st.subheader("Historic Data")
+    st.dataframe(data.loc[:,display_header],use_container_width=True)
 
-        model = joblib.load(r"C:\Users\Anand\Desktop\Coding\projects\stock_market_prediction\models\RandomForrest.pkl")
-        last_date_data = processed_data.iloc[-1]
-        last_date_data = [last_date_data.loc[predictors].values]
-        predicted_value = model.predict(last_date_data)
+with charts:    
 
-        st.subheader("Tommorow Prediction Random Forrest ")
-        if(predicted_value[0]==0):
-            st.info("Price Goes Down",  icon=":material/thumb_down:")      
-        else:
-            st.info("Price Goes Up",  icon=":material/thumb_up:")
+    st.subheader("Prices Variation")
+    st.line_chart(data.loc[:,['Close','Open']])
+                        
+    fig = go.Figure(data=[go.Candlestick(x=date,
+            open=data['Open'],
+            high=data['High'],
+            low=data['Low'],
+            close=data['Close'])])
+
+    st.plotly_chart(fig)
+    
+    processed_data = pre_process_data(data)
+    predictors = processed_data.columns.tolist()
+    predictors.remove("Tomorrow")
+    predictors.remove("Price Change")
+    
+    model = RandomForestClassifier(n_estimators=len(predictors), min_samples_split=len(predictors), random_state=1)
+
+    model = joblib.load(r"C:\Users\Anand\Desktop\Coding\projects\stock_market_prediction\models\RandomForrest.pkl")
+    last_date_data = processed_data.iloc[-1]
+    last_date_data = [last_date_data.loc[predictors].values]
+    predicted_value = model.predict(last_date_data)
+
+    st.subheader("Trend Tommorow Prediction By Random Forrest ")
+    if(predicted_value[0]==0):
+        st.info("Price Goes Down",  icon=":material/thumb_down:")      
+    else:
+        st.info("Price Goes Up",  icon=":material/thumb_up:")
